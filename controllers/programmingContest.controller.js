ff019c4cbe136f85671586dd3c55b849c45736d4
anikatahsin20 <anika.tahsin1320@gmail.com>
2021-08-06 13:56:56 +0600

    test

--- controllers/programmingContest.controller.js
+++ controllers/programmingContest.controller.js
@@ -1,105 +1,109 @@
 const ProgrammingContest = require("../models/ProgrammingContest.model");
-
-const getPC = (req, res) => {
-  res.render("programming-contest/register.ejs", { error: req.flash("error") });
+const getPC = (req, res) =>{
+res.render("programming-contest/register.ejs", {error: req.flash("error")});
 };
 
-const postPC = (req, res) => {
-  const { teamname, category, contact, email, institution, tshirt } = req.body;
-  console.log(teamname);
-  console.log(category);
-  console.log(contact);
-  console.log(email);
-  console.log(institution);
-  console.log(tshirt);
+const postPC = (req,res) =>{
+    const {team_name,name_one, name_two, category, contact, email, institution, tshirt_one, tshirt_two} = req.body;
+    console.log(team_name);
+    console.log(name_one);
+    console.log(name_two);
+    console.log(category);
+    console.log(contact);
+    console.log(email);
+    console.log(institution);
+    console.log(tshirt_one);
+    console.log(tshirt_two);
 
-  let registrationFee = 0;
-  if (category == "School") {
-    registrationFee = 250;
-  } else if (category == "College") {
-    registrationFee = 400;
-  } else {
-    registrationFee = 500;
-  }
-
-  const total = registrationFee;
-  const paid = 0;
-  const selected = false;
-
-  let error = "";
-
-  ProgrammingContest.findOne({ teamname: teamname, contact: contact }).then((participant) => {
-    if (participant) {
-      error = "Participant with this name and contact number already exists!";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/register");
-    } else {
-      const participant = new ProgrammingContest({
-        teamname,
-        category,
-        contact,
-        email,
-        institution,
-        paid,
-        total,
-        selected,
-        tshirt,
-      });
-      
-      participant
-        .save()
-        .then(() => {
-          error = "Participant has been registered successfully!";
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/register");
-        })
-        .catch(() => {
-          error = "An unexpected error occured while registering participant";
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/register");
-        });
+    let registrationFee = 0;
+    if(category=="School"){
+        registrationFee=500;
+    }else if(category=="College"){
+        registrationFee=600;
+    }else{
+        registrationFee=800;
     }
-  });
-};
 
-const getPCList = (req, res) => {
-  let all_participant = [];
-  let error = "";
-  ProgrammingContest.find()
-    .then((data) => {
-      all_participant = data;
-      res.render("programming-contest/list.ejs", {
-        error: req.flash("error"),
-        participants: all_participant,
+   const total= registrationFee;
+   const paid=0;
+   const selected=false;
+   
+   let error="";
+   ProgrammingContest.findOne({team_name:team_name,contact:contact}).then((participant)=>{
+       if(participant){
+           error="Participant with this name and contact number already exists!";
+           console.log(error);
+           req.flash("error", error);
+           res.redirect("/ProgrammingContest/register");
+       }else{
+           const participant=new ProgrammingContest({
+               team_name,
+               name_one,
+               name_two,
+               category,
+               contact,
+               email,
+               institution,
+               paid,
+               total,
+               selected,
+               tshirt_one,
+               tshirt_two
+           });
+
+           participant.save().then(()=>{
+            error="Participant has been registered successfully!";
+           console.log(error);
+           req.flash("error", error);
+            res.redirect("/ProgrammingContest/register");
+           }).catch(()=>{
+               error="An unexpected error occured while registering the participant!";
+               req.flash("error", error);
+               res.redirect("/ProgrammingContest/register");
+           })
+       }
+   })
+    
+}
+
+const getPCList = (req,res) =>{
+    let all_participant = [];
+     let error="";
+     ProgrammingContest.find().then((data)=>{
+         all_participant=data;
+        res.render(programming-contest/list.ejs,{
+            error:req.flash("error"),
+            participant:all_participant,
+        });
+
+     }).catch(()=>{
+        error="Failed to fetch data!";
+        res.render("programming-contest/list.ejs",{
+            error:req.flash("error",error),
+            participants:all_participant,
+        });
+     });
+    
+}
+
+const deletePC = (req,res) =>{
+    let error = "";
+
+    ProgrammingContest.deleteOne({ _id: req.params.id })
+      .then(() => {
+        let error = "Data has been deleted successfully!";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      })
+      .catch(() => {
+        let error = "Failed to delete data";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
       });
-    })
-    .catch(() => {
-      error = "Failed to retrieve data!";
-      res.render("programming-contest/list.ejs", {
-        error: req.flash("error", error),
-        participants: all_participant,
-      });
-    });
-};
+}
 
-const deletePC = (req, res) => {
-  let error = "";
-
-  ProgrammingContest.deleteOne({ _id: req.params.id })
-    .then(() => {
-      let error = "Data has been deleted successfully!";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    })
-    .catch(() => {
-      let error = "Failed to delete data";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    });
-};
-
-const paymentDonePC = (req, res) => {
-  const id = req.params.id;
+const paymentDonePC = (req,res) =>{
+    const id = req.params.id;
 
   ProgrammingContest.findOne({ _id: id })
     .then((participant) => {
@@ -114,7 +118,7 @@ const paymentDonePC = (req, res) => {
         .catch(() => {
           let error = "Data could not be updated!";
           req.flash("error", error);
-          res.redirect("/ProgrammingContest/list");
+          res.redirect("/ProgrammingCcontest/list");
         });
     })
     .catch(() => {
@@ -122,39 +126,32 @@ const paymentDonePC = (req, res) => {
       req.flash("error", error);
       res.redirect("/ProgrammingContest/list");
     });
-};
 
-const selectPC = (req, res) => {
-  const id = req.params.id;
+}
+const selectPC = (req,res) =>{
+    const id = req.params.id;
+  
+    ProgrammingContest.findOne({ _id: id })
+      .then((participant) => {
+        participant.selected = true;
+        participant
+          .save()
+          .then(() => {
+            let error = "Participant has been selected successfully!";
+            req.flash("error", error);
+            res.redirect("/ProgrammingContest/list");
+          })
+          .catch(() => {
+            let error = "Data could not be updated!";
+            req.flash("error", error);
+            res.redirect("/ProgrammingContest/list");
+          });
+      })
+      .catch(() => {
+        let error = "Data could not be updated!";
+        req.flash("error", error);
+        res.redirect("/ProgrammingContest/list");
+      });
+}
 
-  ProgrammingContest.findOne({ _id: id })
-    .then((participant) => {
-      participant.selected = true;
-      participant
-        .save()
-        .then(() => {
-          let error = "Participant has been selected successfully!";
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/list");
-        })
-        .catch(() => {
-          let error = "Data could not be updated!";
-          req.flash("error", error);
-          res.redirect("/ProgrammingContest/list");
-        });
-    })
-    .catch(() => {
-      let error = "Data could not be updated!";
-      req.flash("error", error);
-      res.redirect("/ProgrammingContest/list");
-    });
-};
-
-module.exports = {
-  getPC,
-  postPC,
-  getPCList,
-  deletePC,
-  paymentDonePC,
-  selectPC,
-};
+module.exports = {getPC, postPC, getPCList, deletePC, paymentDonePC, selectPC};
--- models/ProgrammingContest.model.js
+++ models/ProgrammingContest.model.js
@@ -1,46 +1,58 @@
 const mongoose = require("mongoose");
 const PCSchema = new mongoose.Schema({
-  teamname: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  contact: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: false,
-  },
-  institution: {
-    type: String,
-    required: true,
-  },
-  total: {
-    type: Number,
-    required: true,
-  },
-  paid: {
-    type: Number,
-    required: true,
-  },
-  selected: {
-    type: Boolean,
-    required: true,
-  },
-  tshirt: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+    team_name:{
+        type:String,
+        required:true,
+    },
+    name_one:{
+        type:String,
+        required:true,
+    },
+    name_two:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    contact:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:false,
+    },
+    institution:{
+        type:String,
+        required:true,
+    },
+    total:{
+        type:Number,
+        required:true,
+    },
+    paid:{
+        type:Number,
+        required:true,
+    },
+    selected:{
+        type:Boolean,
+        required:true,
+    },
+    tshirt_one:{
+        type:String,
+        required:true,
+    },
+    tshirt_two:{
+        type:String,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
 });
 
-const ProgrammingContest = mongoose.model("ProgrammingContest", PCSchema);
+const ProgrammingContest = mongoose.model("ProgrammingContest",PCSchema);
 module.exports = ProgrammingContest;
